0c1b898ab9521a8fe095c455f9f9a12844cb23de
alehkiz <alexandresoad@hotmail.com>
2022-02-01 23:39:00 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -55,7 +55,6 @@ class TestConfig(BaseConfig):
 class ProductionConfig(BaseConfig):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = f'postgresql://{BaseConfig._SQLALCHEMY_DATABASE_USERNAME}:{BaseConfig._SQLALCHEMY_DATABASE_PASSWORD}@{BaseConfig._SQLALCHEMY_DATABASE_HOST}:{BaseConfig._SQLALCHEMY_DATABASE_PORT}/{BaseConfig._SQLALCHEMY_DATABASE_NAME_PROD}'
-    # SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or f'postgresql://khtknuvbaeavvq:98b557036b61944f2912ccc6aa07b0c907352da55603ce611bb9b744da9398fa@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dtf9faocttt57'
     SESSION_COOKIE_SECURE = False
     MODE = 'prod'
 # 
